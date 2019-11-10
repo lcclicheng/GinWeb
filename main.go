6b2fb2eb28c68b861ca61d4d7b3d51daaace6543
MrWnag <934105927@qq.com>
2019-11-10 15:00:23 +0800

    重新载入

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main(){
+	//初始化路由
+	router:=gin.Default()
+	//请求分配
+	r1:=router.Group("/abc")
+	{
+		r1.GET("/efg", func(ctx *gin.Context) {
+			ctx.Writer.WriteString("hello")
+		})
+	}
+	router.Run(":8081")
+}
